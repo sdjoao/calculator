a81f32b084a973d95530a78dbb19bafad4710901
sdjoao <sdjoaoasl@hotmail.com>
2024-12-22 11:56:45 -0300

    feat(calculator/home): Finish te calculator logics, i use a function sugesting by gpt eval she makes the math automatic.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,21 @@
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
-const zero = document.getElementById("0");
-const min = document.getElementById("-");
-const max = document.getElementById("+");
-const mult = document.getElementById("X");
-const div = document.getElementById("/");
-const equal = document.getElementById("=");
-const dot = document.getElementById(".");
-const clear = document.getElementById("clear");
 
-const result = document.getElementById("result");
+const clear = document.getElementById("clear");
+var screen = document.getElementById("result");
+
 
 function ClearScreen(){
-    result.value = "";
+    screen.value = "";
 }
 
-function insertInScren(num){
-    result.value += num;
+function insertInScreen(num){
+    screen.value += num;
+}
+
+function callResult(){
+
+    try{
+        screen.value = eval(screen.value);
+    }catch(error){
+        alert("Não é possível adicionar dois operadores lado a lado!")
+    }
 }
